8afe2255e5b1479f8d5786d5aed42e7df0e1ab1e
Petr Kočí <petr.koci@gmail.com>
2013-12-19 21:49:37 +0100

    přesun pdf do společného adresáře data

--- R/rename.R
+++ R/rename.R
@@ -1,7 +1,11 @@
 soubory  <- list.files("..", recursive=T)
-
 soubory  <- soubory[grepl("pdf", soubory)]
 
 for (i in soubory) {
   file.rename(paste("../", i, sep=""), paste("../", substr(i,1,3), substr(i,1,2), substr(i,4,5), ".pdf", sep=""))
 }
+
+soubory  <- list.files("..", recursive=T)
+soubory  <- soubory[grepl("pdf", soubory)]
+
+file.copy(paste("../", soubory, sep=""), "../data/")
